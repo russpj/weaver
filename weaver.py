a1704426f0a5acc0ecafde9476d8baee21bbb954
Russ Paul-Jones <russ@paul-jones.org>
2022-08-02 18:36:26 -0700

    Remove step test in word_used_previously
    It has better performance without the test

--- weaver.py
+++ weaver.py
@@ -82,8 +82,6 @@ class Solver:
 
     def word_used_previously(self, step):
         for previous_step in self.steps:
-            if previous_step.step > step.step:
-                break
             if previous_step.word == step.word:
                 return True
         return False
